7536cc4724065d78d0b9d566ce33f1172ec98218
topaloviccc <lara.topalovic@fer.hr>
2025-11-16 21:37:01 +0100

    Added changing ball speed and comments

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,45 @@
 const canvas = document.getElementById("mycanvas");
 const ctx = canvas.getContext("2d");
 
+// parametri za palicu
 const paddleHeight = 15;
 const paddleWidth = 270;
 const PADDLE_SPEED = 10;
-const BRICKS_TOTAL = 50;
-const BRICKS_IN_ROW = 10;
-const START_BALL_SPEED = 3;
-
-const brickWidth = 60;
-const brickHeigth = 20;
-
-const ballDim = 20;
-
 var paddleX = canvas.width / 2 - paddleWidth / 2;
 var paddleY = canvas.height - 30;
 
-var ballSpeedX = START_BALL_SPEED;
-var ballSpeedY = START_BALL_SPEED;
+// parametri za cigle
+const BRICKS_TOTAL = 50;
+const BRICKS_IN_ROW = 10;
+const START_BALL_SPEED = 3;
+const brickWidth = 60;
+const brickHeigth = 20;
 var bricks = [];
 
+// parametri za lopticu
+const ballDim = 20;
+var ballSpeedX = START_BALL_SPEED;
+var ballSpeedY = START_BALL_SPEED;
 var ballX = paddleX + paddleWidth / 2;
 var ballY = paddleY - ballDim;
-var ballSpeed = START_BALL_SPEED;
 
+// određuje početni smjer loptice
+if (Math.random() < 0.5) {
+	ballSpeedX *= -1;
+}
+
+// stanja za tipke na tipkovnici
 var pressedSpace = false;
 var pressedLeft = false;
 var pressedRight = false;
 
+// početni rezultati (trenutni i najbolji)
 var currScore = 0;
 if (localStorage.getItem("highScore") === null) {
 	localStorage.setItem("highScore", 0);
 }
 
+// funkcije crtanja za palicu, lopticu i cigle
 function drawPaddle() {
 	ctx.fillStyle = "white";
 	ctx.shadowBlur = 20;
@@ -47,6 +54,7 @@ function drawBall() {
 	ctx.fillRect(ballX, ballY, ballDim, ballDim);
 }
 
+// cigle se stvaraju po redovima i svaka cigla se sprema u listu bricks kao [x, y, isVisible, color]
 function createBricks() {
 	const rows = BRICKS_TOTAL / BRICKS_IN_ROW;
 	const fillColors = [
@@ -55,7 +63,7 @@ function createBricks() {
 		"rgb(255, 153, 204)",
 		"rgb(0, 255, 0)",
 		"rgb( 255, 255, 153)",
-	];
+	]; // lista boja po retcima
 	for (let i = 0; i < rows; i++) {
 		for (let j = 0; j < BRICKS_IN_ROW; j++) {
 			let brickX = 60 + j * (brickWidth + 50);
@@ -70,6 +78,7 @@ function createBricks() {
 
 createBricks();
 
+// iscrtavaju se samo one cigle za koje vrijedi da je parametar isVisible == true
 function drawBricks() {
 	for (brick of bricks) {
 		if (brick[3] == true) {
@@ -81,6 +90,7 @@ function drawBricks() {
 	}
 }
 
+// pomicanje loptice uz detekciju kolizija
 function moveBall() {
 	ballX += ballSpeedX;
 	ballY -= ballSpeedY;
@@ -89,27 +99,35 @@ function moveBall() {
 	detectBrickCollision();
 }
 
+// pomicanje palice
 function movePaddle() {
+	// pomicanje palice lijevo do ruba okvira
 	if (paddleX > 0 && pressedLeft) {
 		paddleX -= PADDLE_SPEED;
 	}
+	// pomicanje palice desno do ruba okvira
 	if (pressedRight && paddleX + paddleWidth < canvas.width) {
 		paddleX += PADDLE_SPEED;
 	}
 }
 
+// kolizija s rubovima okvira
 function detectEdgesColision() {
+	// kolizija s desnim ili lijevim rubom
 	if (ballX + ballDim >= canvas.width || ballX < 0) {
 		ballSpeedX = -ballSpeedX;
 	}
+	// kolizija s gornjim rubom
 	if (ballY <= 0) {
 		ballSpeedY = -ballSpeedY;
 	}
+	// ako loptica padne ispod donjeg ruba, igra završava i ispisuje se game over
 	if (ballY + ballDim >= canvas.height) {
 		gameOver();
 	}
 }
 
+// kolizija s palicom (x koordinate loptice moraju biti unutar x koordinata palice a donji rub loptice mora se poklapati s gornjim rubom palice)
 function detectPaddleColision() {
 	if (
 		ballX + ballDim >= paddleX &&
@@ -120,11 +138,14 @@ function detectPaddleColision() {
 	}
 }
 
+// kolizija s ciglama
 function detectBrickCollision() {
+	// provjera kolizije za svaku vidljivu ciglu iz liste bricks[]
 	for (brick of bricks) {
 		let brickX = brick[0];
 		let brickY = brick[1];
 		if (brick[3] == true) {
+			// ako je pogođena cigla, rezultat se povećava i mijenja se brzina i smjer loptice te cigla postaje nevidljiva
 			if (
 				ballX + ballDim >= brickX &&
 				ballX <= brickX + brickWidth &&
@@ -132,6 +153,7 @@ function detectBrickCollision() {
 				ballY + ballDim >= brickY
 			) {
 				ballSpeedY = -ballSpeedY;
+				ballSpeedX += (Math.random() - 0.4) * 0.5;
 				brick[3] = false;
 				currScore++;
 			}
@@ -139,7 +161,9 @@ function detectBrickCollision() {
 	}
 }
 
+// funkcije za završetak igre, spremaju najbolji rezultat
 function gameOver() {
+	// prikazuje poruku kad je igra izgubljena
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.font = "bold 40px Verdana";
 	ctx.textAlign = "center";
@@ -151,6 +175,7 @@ function gameOver() {
 }
 
 function wonGame() {
+	// prikazuje poruku kada je igra pobijeđena i sve su cigle uništene
 	if (currScore == BRICKS_TOTAL) {
 		ctx.font = "bold 40px Verdana";
 		ctx.textAlign = "center";
@@ -165,6 +190,7 @@ function wonGame() {
 	}
 }
 
+// prikaz trenutnog rezultata
 function displayCurrScore() {
 	ctx.font = "16px Verdana";
 	ctx.textBaseline = "middle";
@@ -174,12 +200,14 @@ function displayCurrScore() {
 	ctx.fillText("Trenutni broj bodova: " + currScore, 20, 20);
 }
 
+// sprema rezultat trenutne igre ako je bolji od svih prethodnih rezultata
 function saveBestScore() {
 	if (currScore > localStorage.getItem("highScore")) {
 		localStorage.setItem("highScore", currScore);
 	}
 }
 
+// prikaz najboljeg rezultata iz localStoragea
 function displayBestScore() {
 	const highScore = localStorage.getItem("highScore");
 	ctx.font = "16px Verdana";
@@ -194,6 +222,7 @@ function displayBestScore() {
 	);
 }
 
+// početni ekran
 function drawStartScreen() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.font = "bold 36px Verdana";
@@ -209,6 +238,7 @@ function drawStartScreen() {
 	);
 }
 
+// funkcije za detekciju tipki na tipkovnici - space započinje igru, A i D pomiču palicu
 function startGame(e) {
 	let key = e.key.toLowerCase();
 	switch (key) {
@@ -241,6 +271,7 @@ function keyIsUp(e) {
 	}
 }
 
+// glavna funkcija
 function refreshPage() {
 	drawStartScreen();
 	if (pressedSpace) {
